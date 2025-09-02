e102706bb600021ba845ebce54769a1bdec95d29
Helena <helena200805@gmail.com>
2025-09-02 20:16:02 +0700

    seventh commit

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
 let input = [1, 4, 6, 7];
 
 let total = [];
-const result = (n) => {
-    for(let i = 0; i < n.length; i++) {
-        score = n[i] * 2;
-        total.push(score);
+
+for(let i = 0; i < input.length; i++) {
+    score = input[i] * 2;
+    total.push(score);
+}
+
+let output = [];
+for(let i = 0; i < total.length; i++) {
+    const a = total[i];
+    const b = total[i + 1];
+
+    if(a > b) {
+        output.push(a)
     }
 }
 
-// let output = result(input);
-
-console.log(total);
+console.log(output);
