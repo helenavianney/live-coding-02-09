ec309a12d8559669df9acce5ea51c6d7952b1e88
Helena <helena200805@gmail.com>
2025-09-02 20:21:19 +0700

    done

--- main.js
+++ main.js
@@ -2,19 +2,21 @@ let input = [1, 4, 6, 7];
 
 let total = [];
 
-for(let i = 0; i < input.length; i++) {
+for(let i = input.length - 1; i < input.length; i--) {
     score = input[i] * 2;
     total.push(score);
 }
 
-let output = [];
-for(let i = 0; i < total.length; i++) {
-    const a = total[i];
-    const b = total[i + 1];
+// let output = [];
+// for(let i = 0; i < total.length; i++) {
+//     const a = total[i];
+//     const b = total[i + 1];
 
-    if(a > b) {
-        output.push(a)
-    }
-}
+//     if(a > b) {
+//         output.push(a);
+//     } else {
+//         output.push(b)
+//     }
+// }
 
-console.log(output);
+console.log(total);
