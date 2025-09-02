05db99e4680c33b5edd0ffebec5c21de57a0ff47
Helena <helena200805@gmail.com>
2025-09-02 19:48:19 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,11 @@
+let input = [1, 4, 6, 7];
+
+const result = (n) => {
+    let total;
+    for(let i = 0; i <= input.length; i++) {
+        total = input[i] * 2;
+    }
+    return total;
+}
+
+console.log(total);
